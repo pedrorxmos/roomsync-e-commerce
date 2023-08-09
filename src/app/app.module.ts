6b2f65626bc4b03750e936a66aec485cdc36e9d7
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-09 13:56:00 +0200

    🎨 feat(strc): improve app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TopbarComponent } from './components/ui/topbar/topbar.component';
-import { FooterComponent } from './components/ui/footer/footer.component';
-import { ProductCardComponent } from './components/product/product-card/product-card.component';
-import { ProductSliderComponent } from './components/product/product-slider/product-slider.component';
-import { CartItemComponent } from './components/cart/cart-item/cart-item.component';
-import { DropdownComponent } from './components/ui/dropdown/dropdown.component';
-import { StarsRateComponent } from './components/ui/stars-rate/stars-rate.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
-import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
-import { CartPageComponent } from './pages/cart-page/cart-page.component';
-import { CategoryComponent } from './components/ui/category/category.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import {
+  TopbarComponent,
+  FooterComponent,
+  ProductCardComponent,
+  ProductSliderComponent,
+  CartItemComponent,
+  DropdownComponent,
+  StarsRateComponent,
+  CategoryComponent
+} from './components'
+import {
+  HomePageComponent,
+  ProductListPageComponent,
+  ProductDetailPageComponent,
+  CartPageComponent
+} from './pages'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent,
-    FooterComponent,
-    ProductCardComponent,
-    ProductSliderComponent,
     CartItemComponent,
-    DropdownComponent,
-    StarsRateComponent,
-    HomePageComponent,
-    ProductListPageComponent,
-    ProductDetailPageComponent,
     CartPageComponent,
-    CategoryComponent
+    CategoryComponent,
+    DropdownComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProductCardComponent,
+    ProductDetailPageComponent,
+    ProductListPageComponent,
+    ProductSliderComponent,
+    StarsRateComponent,
+    TopbarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
