caeed292a56dee4d8bf0459366d874b6c5c5fa85
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-09 23:43:54 +0200

    ✨ feat(icon): add icon structure and script

--- src/app/components/ui/icon/icon.component.ts
+++ src/app/components/ui/icon/icon.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core'
+import { innerSvg } from './svg.icons'
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.scss']
 })
-export class IconComponent {
+export class IconComponent implements AfterViewInit {
+  @Input()
+  public name: string = 'arrow-down'
 
+  @ViewChild('icon') iconElement!: ElementRef
+
+  ngAfterViewInit(): void {
+    this.iconElement.nativeElement.innerHTML = innerSvg.filter(
+      (svg) => svg.name === this.name
+    )[0].value
+  }
 }
