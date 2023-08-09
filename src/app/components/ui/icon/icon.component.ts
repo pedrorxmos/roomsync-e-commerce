47d03051b351c7a5ae20a2ca0fa02634cd6a0885
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-09 23:53:54 +0200

    🔨 feat(icon): implements icon props

--- src/app/components/ui/icon/icon.component.ts
+++ src/app/components/ui/icon/icon.component.ts
@@ -16,11 +16,24 @@ export class IconComponent implements AfterViewInit {
   @Input()
   public name: string = 'arrow-down'
 
+  @Input()
+  public title?: string
+
+  @Input()
+  public size: string = 'md'
+
+  @Input()
+  public className?: string
+
+  public inlineClass = `icon icon-${this.size} feather feather-airplay ${
+    this.className ? this.className : ''
+  }`
+
   @ViewChild('icon') iconElement!: ElementRef
 
   ngAfterViewInit(): void {
-    this.iconElement.nativeElement.innerHTML = innerSvg.filter(
-      (svg) => svg.name === this.name
-    )[0].value
+    this.iconElement.nativeElement.innerHTML = `${
+      this.title ? `<title>${this.title}</title>` : ''
+    }${innerSvg.filter((svg) => svg.name === this.name)[0].value}`
   }
 }
