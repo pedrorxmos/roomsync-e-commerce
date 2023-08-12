bef5ded9678e56a563681f75f0059b6e8d3c0eef
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-12 19:41:43 +0200

    ✨ feat(topbar): open search func on small devices

--- src/app/components/ui/topbar/topbar.component.ts
+++ src/app/components/ui/topbar/topbar.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild, ElementRef } from '@angular/core'
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
-export class TopbarComponent {}
+export class TopbarComponent {
+  @ViewChild('searchBar') private searchBar!: ElementRef
+
+  onToggleSearch() {
+    this.searchBar.nativeElement.classList.toggle('open')
+  }
+}
