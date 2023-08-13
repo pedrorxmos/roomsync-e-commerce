d44025181ed5b536df45d66e9fb288d9238e59e7
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-13 14:43:53 +0200

    🔨 feat(cat): get categories from json db

--- src/app/components/ui/category/category.component.ts
+++ src/app/components/ui/category/category.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'
+import { Category } from 'src/app/interfaces'
 
 @Component({
   selector: 'app-category',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
-
+  @Input({ required: true })
+  public category!: Category
 }
