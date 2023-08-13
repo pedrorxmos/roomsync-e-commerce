d18ed953844835cbbfbdbefa324d0c4813da50b5
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-13 17:57:45 +0200

    ✨ 💄 feat(cat): add category dynamic style and svg

--- src/app/components/ui/category/category.component.ts
+++ src/app/components/ui/category/category.component.ts
@@ -1,12 +1,29 @@
-import { Component, Input } from '@angular/core'
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild
+} from '@angular/core'
 import { Category } from 'src/app/interfaces'
+import { categorySvg } from './category.icons'
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent {
+export class CategoryComponent implements AfterViewInit {
   @Input({ required: true })
   public category!: Category
+
+  @ViewChild('svg') svgElement!: ElementRef
+
+  ngAfterViewInit(): void {
+    this.svgElement.nativeElement.innerHTML = `<title>${
+      this.category.name
+    }</title> ${
+      categorySvg.filter((svg) => svg.name === this.category.id)[0].value
+    }`
+  }
 }
