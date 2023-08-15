c5c5b33e4d93bf49d369d49c6fabd096f51ddc02
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-15 20:21:43 +0200

    💄 feat(card): add product card html

--- src/app/components/product/product-card/product-card.component.ts
+++ src/app/components/product/product-card/product-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { Product } from 'src/app/interfaces'
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
+  @Input({ required: true })
+  public product!: Product
 
+  public imgUrl = ''
+
+  ngOnInit(): void {
+    this.imgUrl = `assets/img/product/${this.product.subcategory}/${this.product.id}.webp`
+  }
 }
