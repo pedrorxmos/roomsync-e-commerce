d94af505fe82c3868adb9bedbbc95518a2ce16a4
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-16 21:38:52 +0200

    ✨ feat(cart): add recommended products

--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import productsDB from '../../../database/products.json'
+import { Product } from 'src/app/interfaces'
 
 @Component({
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.scss']
 })
-export class CartPageComponent {
-  public recomendedProducts = []
+export class CartPageComponent implements OnInit {
+  public recomendedProducts: Product[] = []
+
+  ngOnInit(): void {
+    this.recomendedProducts = productsDB
+      .sort((a, b) => 0.5 - Math.random())
+      .slice(0, 8) as Product[]
+  }
 }
