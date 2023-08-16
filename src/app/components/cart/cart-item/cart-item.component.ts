10607c4172fa70f98edafe413397bf4b5daf8f24
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-16 21:43:45 +0200

    ✨ feat(cart): add cart products

--- src/app/components/cart/cart-item/cart-item.component.ts
+++ src/app/components/cart/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'
+import { Cart } from 'src/app/interfaces'
 
 @Component({
   selector: 'app-cart-item',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent {
-
+  @Input()
+  public product!: Cart
 }
--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core'
+import { AfterContentChecked, Component, OnInit } from '@angular/core'
 import productsDB from '../../../database/products.json'
-import { Product } from 'src/app/interfaces'
+import { Cart, Product } from 'src/app/interfaces'
+import { getCart } from 'src/app/helpers'
 
 @Component({
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.scss']
 })
-export class CartPageComponent implements OnInit {
+export class CartPageComponent implements OnInit, AfterContentChecked {
+  public products: Cart[] = getCart()
   public recomendedProducts: Product[] = []
 
   ngOnInit(): void {
     this.recomendedProducts = productsDB
       .sort((a, b) => 0.5 - Math.random())
       .slice(0, 8) as Product[]
+  }
+
+  ngAfterContentChecked(): void {
+    this.products = getCart()
   }
 }
