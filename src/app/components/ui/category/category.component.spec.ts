263b901c4b7782cd7e396e0e076bd6eb1453fe11
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-20 17:17:42 +0200

    ✅ feat(test): add category component tests

--- src/app/components/ui/category/category.component.spec.ts
+++ src/app/components/ui/category/category.component.spec.ts
@@ -1,11 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick
+} from '@angular/core/testing'
 
 import { CategoryComponent } from './category.component'
 import { AppModule } from 'src/app/app.module'
+import { category, decorationInnerSvg } from 'src/app/fixtures/category.fixture'
+import { Location } from '@angular/common'
 
 describe('CategoryComponent', () => {
   let component: CategoryComponent
   let fixture: ComponentFixture<CategoryComponent>
+  let location: Location
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -20,4 +28,46 @@ describe('CategoryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
+  it('should show title and subtitle', () => {
+    const compiled = fixture.nativeElement as HTMLElement
+    component.category = category
+    component.ngAfterViewInit()
+    fixture.detectChanges()
+
+    expect(
+      compiled.querySelector('.category-card .card-info')?.textContent
+    ).toContain(category.name)
+
+    expect(
+      compiled.querySelector('.category-card .card-info')?.textContent
+    ).toContain(category.parent)
+  })
+
+  it('should render svg icon', () => {
+    const compiled = fixture.nativeElement as HTMLElement
+    component.category = category
+    component.ngAfterViewInit()
+    fixture.detectChanges()
+
+    console.log(compiled)
+    expect(
+      compiled.querySelector('.category-card .card-icon svg')?.innerHTML
+    ).toContain(decorationInnerSvg)
+  })
+
+  it('should redirect to category page', fakeAsync(() => {
+    location = TestBed.inject(Location)
+    component.category = category
+    component.ngAfterViewInit()
+    fixture.detectChanges()
+
+    const btn =
+      fixture.debugElement.nativeElement.querySelector('.category-card')
+    btn.click()
+    tick()
+    fixture.detectChanges()
+
+    expect(location.path()).toBe(`/shop/${category.id}`)
+  }))
 })
