3ee2587151215025e03228d87c09ab65a73df121
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-20 20:57:21 +0200

    ✅ feat(tests): filter component tests

--- src/app/components/product/product-filter/product-filter.component.spec.ts
+++ src/app/components/product/product-filter/product-filter.component.spec.ts
@@ -20,4 +20,51 @@ describe('ProductFilterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
+  it('should show title', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    component.title = 'Color'
+    fixture.detectChanges()
+
+    expect(compiled.querySelector('.filter-dropdown')?.textContent).toContain(
+      'Color'
+    )
+    expect(
+      compiled.querySelector('.filter-options__header')?.textContent
+    ).toContain('Color')
+  })
+
+  it('should open and close (with close btn) itself', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    component.title = 'color'
+    fixture.detectChanges()
+
+    fixture.debugElement.nativeElement.querySelector('.filter-dropdown').click()
+    expect(
+      compiled.querySelector('.product-filter')?.classList.contains('open')
+    ).toBeTruthy()
+
+    fixture.debugElement.nativeElement
+      .querySelector('.filter-options__header__action')
+      .click()
+    expect(
+      compiled.querySelector('.product-filter')?.classList.contains('open')
+    ).toBeFalsy()
+  })
+
+  it('should open and close (with overlay) itself', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    component.title = 'color'
+    fixture.detectChanges()
+
+    fixture.debugElement.nativeElement.querySelector('.filter-dropdown').click()
+    expect(
+      compiled.querySelector('.product-filter')?.classList.contains('open')
+    ).toBeTruthy()
+
+    fixture.debugElement.nativeElement.querySelector('.filter-overlay').click()
+    expect(
+      compiled.querySelector('.product-filter')?.classList.contains('open')
+    ).toBeFalsy()
+  })
 })
