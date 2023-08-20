5027ec2ebd3f990d874b0018b7fc4264f4aec1bc
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-20 20:41:26 +0200

    ✅ feat(test): home page tests

--- src/app/pages/home-page/home-page.component.spec.ts
+++ src/app/pages/home-page/home-page.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { HomePageComponent } from './home-page.component'
 import { AppModule } from 'src/app/app.module'
+import categoriesDB from '../../../database/categories.json'
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent
@@ -19,5 +20,26 @@ describe('HomePageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should show title and subtitle', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    expect(compiled.textContent).toContain('RoomSync')
+    expect(compiled.textContent).toContain('Need a change?')
+  })
+
+  it('should show product slider', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    expect(compiled.querySelector('.home-products')?.innerHTML).toContain(
+      'app-product-slider'
+    )
+  })
+
+  it('should show all categories', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+
+    expect(
+      compiled.querySelectorAll('.categories-list app-category').length
+    ).toBe(categoriesDB.length)
   })
 })
