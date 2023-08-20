835a1bf941b49c0b0beeaf12ac81523f947f20d4
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-20 13:30:55 +0200

    ✅ feat(test): cart page tests

---
+++ src/app/fixtures/cart.fixture.ts
@@ -0,0 +1,26 @@
+export const cartStorage = [
+  {
+    name: 'Green Cactus Vase',
+    id: 'green-cactus-vase',
+    price: 15.99,
+    subcategory: 'bowls-vases',
+    qty: 1
+  }
+]
+
+export const cartStorageMultipleItems = [
+  {
+    name: 'Green Cactus Vase',
+    id: 'green-cactus-vase',
+    price: 15.99,
+    subcategory: 'bowls-vases',
+    qty: 1
+  },
+  {
+    name: 'White Chair Outdoor',
+    id: 'white-chair-outdoor',
+    price: 269.99,
+    subcategory: 'outdoors',
+    qty: 1
+  }
+]
--- src/app/pages/cart-page/cart-page.component.spec.ts
+++ src/app/pages/cart-page/cart-page.component.spec.ts
@@ -2,22 +2,67 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { CartPageComponent } from './cart-page.component'
 import { AppModule } from 'src/app/app.module'
+import {
+  cartStorage,
+  cartStorageMultipleItems
+} from 'src/app/fixtures/cart.fixture'
+import { Cart } from 'src/app/interfaces'
+import { getCart } from 'src/app/helpers'
+import { ActivatedRoute, Router } from '@angular/router'
+import { inject } from '@angular/core'
 
 describe('CartPageComponent', () => {
   let component: CartPageComponent
   let fixture: ComponentFixture<CartPageComponent>
+  let router: Router
 
   beforeEach(() => {
+    localStorage.clear()
     TestBed.configureTestingModule({
       declarations: [CartPageComponent],
       imports: [AppModule]
     })
     fixture = TestBed.createComponent(CartPageComponent)
+    router = TestBed.inject(Router)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should show as much cart items as cart products in localstorage', () => {
+    localStorage.setItem('cart', JSON.stringify(cartStorage))
+    component.ngAfterContentChecked()
+    expect(component.products).toEqual(cartStorage)
+  })
+
+  it('should calculate subtotal of cart', () => {
+    localStorage.setItem('cart', JSON.stringify(cartStorageMultipleItems))
+    component.ngAfterContentChecked()
+    let subtotal = 0
+
+    cartStorageMultipleItems.forEach((prod: Cart) => {
+      subtotal += prod.price * prod.qty
+    })
+
+    expect(component.subtotal).toEqual(subtotal)
+  })
+
+  it('should remove cart items and localStorage on checkout', () => {
+    localStorage.setItem('cart', JSON.stringify(cartStorageMultipleItems))
+    component.ngAfterContentChecked()
+    expect(component.products).toEqual(cartStorageMultipleItems)
+
+    component.onRemoveAll()
+    expect(getCart()).toEqual([])
+  })
+
+  it('should change route on checkout', () => {
+    const navigateByUrlSpy = spyOn(router, 'navigateByUrl')
+    component.onRemoveAll()
+
+    expect(navigateByUrlSpy).toHaveBeenCalledWith('/')
   })
 })
