ff6e83e28de11f5dabb417c24bc767e592913d98
Pedro Ramos <pedrorxmosdev@gmail.com>
2023-08-22 14:23:33 +0200

    ✅ feat(newsletter): add newsletter notice tests

--- src/app/components/ui/footer/footer.component.spec.ts
+++ src/app/components/ui/footer/footer.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { FooterComponent } from './footer.component'
 import { AppModule } from 'src/app/app.module'
+import { ElementRef } from '@angular/core'
 
 describe('FooterComponent', () => {
   let component: FooterComponent
@@ -32,9 +33,54 @@ describe('FooterComponent', () => {
   })
 
   it('should render copyright notice', () => {
-    const compiled = fixture.nativeElement as HTMLElement
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
     expect(compiled.querySelector('.copyright-text')?.textContent).toEqual(
       '© 2023 RoomSync. All rights reserved.'
     )
+  })
+
+  it('should open newsletter notice if email is correct', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    if (component.input) {
+      component.input.nativeElement.value = 'email@email.es'
+    }
+    component.onNewsletterOpen()
+    fixture.detectChanges()
+
+    expect(
+      compiled.querySelector('.newsletter-notice')?.classList.contains('open')
+    ).toBeTruthy()
+  })
+
+  it('should not open newsletter notice if email is invalid and add error', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    if (component.input) {
+      component.input.nativeElement.value = 'wrongemail'
+    }
+    component.onNewsletterOpen()
+    fixture.detectChanges()
+
+    expect(
+      compiled.querySelector('.newsletter-error')?.classList.contains('error')
+    ).toBeTruthy()
+
+    expect(
+      compiled.querySelector('.newsletter-notice')?.classList.contains('open')
+    ).toBeFalsy()
+  })
+
+  it('should close newsletter when close', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement
+    compiled.querySelector('.newsletter-notice')?.classList.add('open')
+
+    expect(
+      compiled.querySelector('.newsletter-notice')?.classList.contains('open')
+    ).toBeTruthy()
+
+    component.onNewsletterClose()
+
+    expect(
+      compiled.querySelector('.newsletter-notice')?.classList.contains('open')
+    ).toBeFalsy()
   })
 })
